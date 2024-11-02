df76d0ea3e81566c60e7d602a9719636697734a3
jcdkiki <63550535+jcdkiki@users.noreply.github.com>
2024-11-02 10:21:10 +0000

    Deploying to gh-pages from @ jcdkiki/computer-algebra-system@1873cf5cb676668b502e6b074ecbb10c2bfe7813 🚀

--- classInteger.js
+++ classInteger.js
@@ -2,6 +2,7 @@ var classInteger =
 [
     [ "Integer", "classInteger.html#a7fcab0564d7270017ebcca55bae1a17a", null ],
     [ "Integer", "classInteger.html#a9e8bf8c72458dff4ceb5d6cdf9e5c97a", null ],
+    [ "Integer", "classInteger.html#acde8b5f47d3081f5c359d21449669756", null ],
     [ "asString", "classInteger.html#aaeb4cc3b4e4bf39322a49130a6c58c45", null ],
     [ "operator bool", "classInteger.html#ae8fc1288d02b104e8c0fd9b05bed948f", null ],
     [ "operator<<", "classInteger.html#a5921c24569de78d9d4e9214926761484", null ],
--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['integer_0',['Integer',['../classInteger.html',1,'Integer'],['../classInteger.html#a7fcab0564d7270017ebcca55bae1a17a',1,'Integer::Integer()'],['../classInteger.html#a9e8bf8c72458dff4ceb5d6cdf9e5c97a',1,'Integer::Integer(const char *str)']]],
+  ['integer_0',['Integer',['../classInteger.html',1,'Integer'],['../classInteger.html#a7fcab0564d7270017ebcca55bae1a17a',1,'Integer::Integer()'],['../classInteger.html#a9e8bf8c72458dff4ceb5d6cdf9e5c97a',1,'Integer::Integer(const char *str)'],['../classInteger.html#acde8b5f47d3081f5c359d21449669756',1,'Integer::Integer(const Natural &amp;number)']]],
   ['integer_2ehpp_1',['integer.hpp',['../integer_8hpp.html',1,'']]]
 ];

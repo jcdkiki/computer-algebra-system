e03dfd565e89b2893ef06bc4c332ee03474bd3c5
Gorsky Kirill <kirillgorskiy@gmail.com>
2024-11-03 00:23:38 +0300

    wip

--- cas/main.cpp
+++ cas/main.cpp
@@ -1,19 +1,71 @@
-#include <iostream>
-#include "natural.hpp"
-#include "integer.hpp"
-
-int main()
+#include <wx/gdicmn.h>
+#include <wx/sizer.h>
+#include <wx/stringimpl.h>
+#include <wx/wx.h>
+ 
+class MyApp : public wxApp
 {
-    Natural number1, number2;
-    Integer number3, number4;
-    while (true) {
-        std::cout << "Type two natural numbers: " << std::endl;
-        std::cin >> number1 >> number2;
-        std::cout << "You typed: " << number1 << " and " << number2 << std::endl;
+public:
+    virtual bool OnInit();
+};
+ 
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+ 
+private:
+    void OnExit(wxCommandEvent& event);
+    void OnHelp(wxCommandEvent& event);
+};
+ 
+enum
+{
+    ID_Natural
+};
+ 
+wxIMPLEMENT_APP(MyApp);
+ 
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+ 
+MyFrame::MyFrame()
+    : wxFrame(NULL, wxID_ANY, "Hello World", wxDefaultPosition, wxSize(300, 400), wxCAPTION)
+{
+    wxToolBar *toolbar = CreateToolBar();
+    toolbar->AddTool(wxID_HELP, "Help", wxBitmap("data/help.png"));
+    toolbar->AddTool(wxID_HELP, "Natural", wxBitmap("data/plus32.png"));
+    toolbar->AddTool(wxID_HELP, "Integer", wxBitmap("data/plus-minus32.png"));
+    toolbar->AddTool(wxID_HELP, "Rational", wxBitmap("data/percentage32.png"));
+    toolbar->AddTool(wxID_HELP, "Polynomial", wxBitmap("data/x32.png"));
 
-        std::cout << "Type two integer numbers: " << std::endl;
-        std::cin >> number3 >> number4;
-        std::cout << "You typed: " << number3 << " and " << number4 << std::endl;
-    }
 
+    wxTextCtrl *ctrl = new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_READONLY);
+    ctrl->SetSize(300, 32);
+
+    new wxButton(this, wxID_ANY, "1");
+    new wxButton(this, wxID_ANY, "2");
+
+    CreateStatusBar();
+    SetStatusText("Welcome to Computer Algebra System");
+    new wxStaticBoxSizer(wxVERTICAL, this);
+    
+    Bind(wxEVT_MENU, &MyFrame::OnHelp, this, wxID_HELP);
+    //Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    //Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+ 
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+ 
+void MyFrame::OnHelp(wxCommandEvent& event)
+{
+    wxMessageBox("Help message will be placed here...",
+                 "Help", wxOK | wxICON_INFORMATION);
 }

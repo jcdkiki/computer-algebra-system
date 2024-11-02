8024857b9229f840d96fc4b3bd644f1873c658b7
jcdkiki <63550535+jcdkiki@users.noreply.github.com>
2024-11-02 10:06:53 +0000

    Deploying to gh-pages from @ jcdkiki/computer-algebra-system@a1ae3844d8cb9ef55aa931d59e43c0c9d5d31350 🚀

--- natural_8hpp.js
+++ natural_8hpp.js
@@ -1,6 +1,7 @@
 var natural_8hpp =
 [
     [ "Natural", "classNatural.html", "classNatural" ],
+    [ "operator*", "natural_8hpp.html#a33b7b6db3a076468847c41bf6bc420eb", null ],
     [ "operator+", "natural_8hpp.html#aeb3428745d0811066099a6cfb09ef2c4", null ],
     [ "operator-", "natural_8hpp.html#a7202bc20f86303e93687035afe343b89", null ],
     [ "operator<<", "natural_8hpp.html#a5507c0ccac2ef7932f2d0b0bbce238a5", null ],

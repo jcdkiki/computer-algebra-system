d73b2ce88e53e4bfa2d22b832b1a2256498d56a7
jcdkiki <63550535+jcdkiki@users.noreply.github.com>
2024-11-05 18:32:02 +0000

    Deploying to gh-pages from @ jcdkiki/computer-algebra-system@d211561f3d01552d1297549e9317dfdb82d672f7 🚀

--- search/related_2.js
+++ search/related_2.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['operator_2a_0',['operator*',['../classNatural.html#a33b7b6db3a076468847c41bf6bc420eb',1,'Natural']]],
-  ['operator_2b_1',['operator+',['../classInteger.html#ad69a5b95379d5daa962a3f8d9042909b',1,'Integer::operator+()'],['../classNatural.html#aeb3428745d0811066099a6cfb09ef2c4',1,'Natural::operator+()']]],
-  ['operator_2d_2',['operator-',['../classInteger.html#aef3fcc257cf59cc82cf5f6db20959989',1,'Integer::operator-()'],['../classNatural.html#a7202bc20f86303e93687035afe343b89',1,'Natural::operator-()']]],
-  ['operator_2f_3',['operator/',['../classNatural.html#a7134514f505f3986195e31b43325278c',1,'Natural']]],
-  ['operator_3c_3c_4',['operator&lt;&lt;',['../classInteger.html#a5921c24569de78d9d4e9214926761484',1,'Integer::operator&lt;&lt;()'],['../classNatural.html#a5507c0ccac2ef7932f2d0b0bbce238a5',1,'Natural::operator&lt;&lt;()'],['../classPolynomial.html#ae8148672de86961263b190b707d63025',1,'Polynomial::operator&lt;&lt;()'],['../classRational.html#addb0a29270883190578358da0097fbe8',1,'Rational::operator&lt;&lt;()']]],
-  ['operator_3e_3e_5',['operator&gt;&gt;',['../classInteger.html#a992423e53ec0794560e902ebfe04d4bb',1,'Integer::operator&gt;&gt;()'],['../classNatural.html#af50e755a822231379576e166fbb883a3',1,'Natural::operator&gt;&gt;()'],['../classPolynomial.html#a07cf3f3483b39217fd77c7eb99a949fe',1,'Polynomial::operator&gt;&gt;()'],['../classRational.html#afef13f4f6139b89f0ee9d2b255408fd7',1,'Rational::operator&gt;&gt;()']]]
+  ['operator_25_0',['operator%',['../classNatural.html#a10d694b4546539b2d785c354e8fea847',1,'Natural']]],
+  ['operator_2a_1',['operator*',['../classNatural.html#a33b7b6db3a076468847c41bf6bc420eb',1,'Natural']]],
+  ['operator_2b_2',['operator+',['../classInteger.html#ad69a5b95379d5daa962a3f8d9042909b',1,'Integer::operator+()'],['../classNatural.html#aeb3428745d0811066099a6cfb09ef2c4',1,'Natural::operator+()']]],
+  ['operator_2d_3',['operator-',['../classInteger.html#aef3fcc257cf59cc82cf5f6db20959989',1,'Integer::operator-()'],['../classNatural.html#a7202bc20f86303e93687035afe343b89',1,'Natural::operator-()']]],
+  ['operator_2f_4',['operator/',['../classNatural.html#a7134514f505f3986195e31b43325278c',1,'Natural']]],
+  ['operator_3c_3c_5',['operator&lt;&lt;',['../classInteger.html#a5921c24569de78d9d4e9214926761484',1,'Integer::operator&lt;&lt;()'],['../classNatural.html#a5507c0ccac2ef7932f2d0b0bbce238a5',1,'Natural::operator&lt;&lt;()'],['../classPolynomial.html#ae8148672de86961263b190b707d63025',1,'Polynomial::operator&lt;&lt;()'],['../classRational.html#addb0a29270883190578358da0097fbe8',1,'Rational::operator&lt;&lt;()']]],
+  ['operator_3e_3e_6',['operator&gt;&gt;',['../classInteger.html#a992423e53ec0794560e902ebfe04d4bb',1,'Integer::operator&gt;&gt;()'],['../classNatural.html#af50e755a822231379576e166fbb883a3',1,'Natural::operator&gt;&gt;()'],['../classPolynomial.html#a07cf3f3483b39217fd77c7eb99a949fe',1,'Polynomial::operator&gt;&gt;()'],['../classRational.html#afef13f4f6139b89f0ee9d2b255408fd7',1,'Rational::operator&gt;&gt;()']]]
 ];

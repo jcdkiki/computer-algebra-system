30d31d5856598873b8905f0cad42a5dbaed1c6cb
jcdkiki <63550535+jcdkiki@users.noreply.github.com>
2024-11-08 19:33:36 +0000

    Deploying to gh-pages from @ jcdkiki/computer-algebra-system@716f82766df888f0d8d16330a0ab078e7ca43b35 🚀

--- classRational.js
+++ classRational.js
@@ -3,6 +3,11 @@ var classRational =
     [ "Rational", "classRational.html#a3bbd6ff67c590da76ddfcbd48cd3de0a", null ],
     [ "Rational", "classRational.html#a32a09888b437d30cc0c2496845242c2e", null ],
     [ "asString", "classRational.html#af4093366d0d86d21036b8ad3147af274", null ],
+    [ "get_denominator", "classRational.html#ab00dd18c2cf045f2eedc0fb4eeffd184", null ],
+    [ "get_numerator", "classRational.html#adf9b6afbfc81067f1fb65600072ae5b9", null ],
+    [ "operator+=", "classRational.html#ac1d2537646823dc3777f471dd53ca4a0", null ],
+    [ "reduce", "classRational.html#adbc97e55a7fd24a86b3b0b98b9ddddb8", null ],
+    [ "operator+", "classRational.html#ac8cb30f89699c836f45a8b6e66d94f58", null ],
     [ "operator<<", "classRational.html#addb0a29270883190578358da0097fbe8", null ],
     [ "operator>>", "classRational.html#afef13f4f6139b89f0ee9d2b255408fd7", null ]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['getdivdigitinpower_0',['getDivDigitInPower',['../classNatural.html#a56fab5de99caafc6435dda4259a859a7',1,'Natural::getDivDigitInPower()'],['../natural_8hpp.html#a56fab5de99caafc6435dda4259a859a7',1,'getDivDigitInPower():&#160;natural.cpp']]],
-  ['greatcommdiv_1',['greatCommDiv',['../classNatural.html#ad9db39e985a8da26ebd5eea3087aeb6a',1,'Natural::greatCommDiv()'],['../natural_8hpp.html#ad9db39e985a8da26ebd5eea3087aeb6a',1,'greatCommDiv():&#160;natural.cpp']]]
+  ['get_5fdenominator_0',['get_denominator',['../classRational.html#ab00dd18c2cf045f2eedc0fb4eeffd184',1,'Rational']]],
+  ['get_5fnumerator_1',['get_numerator',['../classRational.html#adf9b6afbfc81067f1fb65600072ae5b9',1,'Rational']]],
+  ['getdivdigitinpower_2',['getDivDigitInPower',['../classNatural.html#a56fab5de99caafc6435dda4259a859a7',1,'Natural::getDivDigitInPower()'],['../natural_8hpp.html#a56fab5de99caafc6435dda4259a859a7',1,'getDivDigitInPower():&#160;natural.cpp']]],
+  ['greatcommdiv_3',['greatCommDiv',['../classNatural.html#ad9db39e985a8da26ebd5eea3087aeb6a',1,'Natural::greatCommDiv()'],['../natural_8hpp.html#ad9db39e985a8da26ebd5eea3087aeb6a',1,'greatCommDiv():&#160;natural.cpp']]]
 ];

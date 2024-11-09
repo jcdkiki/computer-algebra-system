c64254cbf3a0e21d81843f44ef11bb7d75c6c3ba
jcdkiki <63550535+jcdkiki@users.noreply.github.com>
2024-11-09 10:42:52 +0000

    Deploying to gh-pages from @ jcdkiki/computer-algebra-system@a8c9443077fec0a35510a37d54ca9547116726ef 🚀

--- classRational.js
+++ classRational.js
@@ -5,9 +5,15 @@ var classRational =
     [ "asString", "classRational.html#af4093366d0d86d21036b8ad3147af274", null ],
     [ "get_denominator", "classRational.html#ab00dd18c2cf045f2eedc0fb4eeffd184", null ],
     [ "get_numerator", "classRational.html#adf9b6afbfc81067f1fb65600072ae5b9", null ],
+    [ "operator*=", "classRational.html#a2cbd4e7a44f88bf62444a9ac16123b17", null ],
     [ "operator+=", "classRational.html#ac1d2537646823dc3777f471dd53ca4a0", null ],
+    [ "operator-=", "classRational.html#ad41e2331e20b0be7eb6fd57d9d81d9e9", null ],
+    [ "operator/=", "classRational.html#af3d9cfd7e9f445399e72a623f1e6b67f", null ],
     [ "reduce", "classRational.html#adbc97e55a7fd24a86b3b0b98b9ddddb8", null ],
+    [ "operator*", "classRational.html#ab0a07584f8c8d442afd20600b837585f", null ],
     [ "operator+", "classRational.html#ac8cb30f89699c836f45a8b6e66d94f58", null ],
+    [ "operator-", "classRational.html#ace4e870ca277ecfe9eb1dffdaf379c50", null ],
+    [ "operator/", "classRational.html#af89e39384f7dbaec15dec35f28ba3df6", null ],
     [ "operator<<", "classRational.html#addb0a29270883190578358da0097fbe8", null ],
     [ "operator>>", "classRational.html#afef13f4f6139b89f0ee9d2b255408fd7", null ]
 ];

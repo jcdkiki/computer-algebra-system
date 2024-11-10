9442a955b9bea7e63eec11bc4e75fbe20205e311
sciengi <notpiterovalexander@gmail.com>
2024-11-10 14:23:55 +0300

    test(P): reduce loops

--- libcas-tst/polynomial.cpp
+++ libcas-tst/polynomial.cpp
@@ -99,19 +99,7 @@ TEST(POLYNOMIAL, ADD)
     {
         poly p1(input1), p2(input2), result = p1 + p2;
         ASSERT_EQ(result.asString(), expected);
-    }
 
-    for (auto [input1, input2, expected] : {
-        tuple { "0", "0", "0" },
-        tuple { "1 + x^2", "7", "8 + x^2" },
-        tuple { "2 + x^3", "3 + 5x^3", "5 + 6x^3" },
-        tuple { "19x", "1 + 20x + 3x^2", "1 + 39x + 3x^2" },
-        tuple { "19x^2", "1 + x^3", "1 + 19x^2 + x^3" },
-        tuple { "-x^3", "x^3", "0" },
-        tuple { "2x^2 + x^3", "2x^2 - x^3", "4x^2" },
-    })
-    {
-        poly p1(input1), p2(input2);
         p1 += p2;
         ASSERT_EQ(p1.asString(), expected);
     }
@@ -134,19 +122,7 @@ TEST(POLYNOMIAL, SUB)
     {
         poly p1(input1), p2(input2), result = p1 - p2;
         ASSERT_EQ(result.asString(), expected);
-    }
-
-    for (auto [input1, input2, expected] : {
-        tuple { "0", "0", "0" },
-        tuple { "1 + x^2", "7", "-6 + x^2" },
-        tuple { "2 + x^3", "3 + 5x^3", "-1 - 4x^3" },
-        tuple { "19x", "1 + 20x + 3x^2", "-1 - x - 3x^2" },
-        tuple { "19x^2", "1 + x^3", "-1 + 19x^2 - x^3" },
-        tuple { "x^3", "x^3", "0" },
-        tuple { "-2x^2 + x^3", "2x^2 + x^3", "-4x^2" },
-    })
-    {
-        poly p1(input1), p2(input2);
+        
         p1 -= p2;
         ASSERT_EQ(p1.asString(), expected);
     }
